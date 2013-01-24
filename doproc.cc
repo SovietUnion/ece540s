26b536f7fe0ce18046b50b9bc51a3844e2651791
sunyao.xjtu@gmail.com <sunyao.xjtu@gmail.com@3b75b716-00b5-292a-5db9-74986e908250>
2013-01-24 03:46:20 +0000

    initial

---
+++ doproc.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+extern "C" { 
+#include <simple.h>
+}
+
+#include<vector>
+#include<hash_map>
+
+
+simple_instr* do_procedure (simple_instr *inlist, char *proc_name)
+{
+    // build flow control graph 
+
+    // find immediate dominators    
+
+    return inlist;
+}
